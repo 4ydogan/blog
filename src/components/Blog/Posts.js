5faef3ae5085a99d0f76ae59c8209fa3dc58dbc9
4ydogan <mustafaaydogan12@outlook.com>
2022-06-17 22:25:56 +0300

    Some updates

--- src/components/Blog/Posts.js
+++ src/components/Blog/Posts.js
@@ -9,215 +9,26 @@ class Posts extends Component {
 
 	async componentDidMount() {
 		this.setState({ loading: true });
-		const response = await axios.get(`/blogpost/1`);
-		this.setState({ content: response.data });
+		const response = await axios.get(`/blogpost/all`);
+		this.setState({ posts: response.data });
 		this.setState({ loading: false });
+	}
+
+	allPosts() {
+		let posts = this.state.posts?.map(content => {
+			return <Post key={content.id} content={content} />
+		});
+
+		return posts;
 	}
 
 	render() {
 
 		return (
 			<>
-				{ this.state.loading ? null : 
+				{this.state.loading ? null :
 					<div className="row gy-4">
-						<Post content={this.state.content} />
-						<div className="col-sm-6">
-							<div className="post post-grid rounded bordered">
-								<div className="thumb top-rounded">
-									<a href="category.html" className="category-badge position-absolute">Inspiration</a>
-									<a href="blog-single.html">
-										<div className="inner">
-											<img src="https://katen-template.netlify.app/html/images/posts/post-md-1.jpg" alt="post-title" />
-										</div>
-									</a>
-								</div>
-								<div className="details">
-									<ul className="meta list-inline mb-0">
-										<li className="list-inline-item"><a href="#"><img src="images/other/author-sm.png" className="author" alt="author" />Katen Doe</a></li>
-										<li className="list-inline-item">29 March 2021</li>
-									</ul>
-									<h5 className="post-title mb-3 mt-3"><a href="blog-single.html">Most Important Thing You Need To Know About Swim</a></h5>
-									<p className="excerpt mb-0">I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence.</p>
-								</div>
-							</div>
-						</div>
-
-						<div className="col-sm-6">
-							<div className="post post-grid rounded bordered">
-								<div className="thumb top-rounded">
-									<a href="category.html" className="category-badge position-absolute">Fashion</a>
-									<a href="blog-single.html">
-										<div className="inner">
-											<img src="https://katen-template.netlify.app/html/images/posts/post-md-1.jpg" alt="post-title" />
-										</div>
-									</a>
-								</div>
-								<div className="details">
-									<ul className="meta list-inline mb-0">
-										<li className="list-inline-item"><a href="#"><img src="images/other/author-sm.png" className="author" alt="author" />Katen Doe</a></li>
-										<li className="list-inline-item">29 March 2021</li>
-									</ul>
-									<h5 className="post-title mb-3 mt-3"><a href="blog-single.html">The Secrets To Finding Class Tools For Your Dress</a></h5>
-									<p className="excerpt mb-0">I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence.</p>
-								</div>
-							</div>
-						</div>
-
-						<div className="col-sm-6">
-							<div className="post post-grid rounded bordered">
-								<div className="thumb top-rounded">
-									<a href="category.html" className="category-badge position-absolute">Lifestyle</a>
-									<span className="post-format">
-										<i className="icon-camrecorder"></i>
-									</span>
-									<a href="blog-single.html">
-										<div className="inner">
-											<img src="https://katen-template.netlify.app/html/images/posts/post-md-1.jpg" alt="post-title" />
-										</div>
-									</a>
-								</div>
-								<div className="details">
-									<ul className="meta list-inline mb-0">
-										<li className="list-inline-item"><a href="#"><img src="images/other/author-sm.png" className="author" alt="author" />Katen Doe</a></li>
-										<li className="list-inline-item">29 March 2021</li>
-									</ul>
-									<h5 className="post-title mb-3 mt-3"><a href="blog-single.html">How I Improved My Fashion Style In One Day</a></h5>
-									<p className="excerpt mb-0">I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence.</p>
-								</div>
-							</div>
-						</div>
-						<div className="col-sm-6">
-							<div className="post post-grid rounded bordered">
-								<div className="thumb top-rounded">
-									<a href="category.html" className="category-badge position-absolute">Trending</a>
-									<a href="blog-single.html">
-										<div className="inner">
-											<img src="https://katen-template.netlify.app/html/images/posts/post-md-1.jpg" alt="post-title" />
-										</div>
-									</a>
-								</div>
-								<div className="details">
-									<ul className="meta list-inline mb-0">
-										<li className="list-inline-item"><a href="#"><img src="images/other/author-sm.png" className="author" alt="author" />Katen Doe</a></li>
-										<li className="list-inline-item">29 March 2021</li>
-									</ul>
-									<h5 className="post-title mb-3 mt-3"><a href="blog-single.html">3 Easy Ways To Make Your iPhone Faster</a></h5>
-									<p className="excerpt mb-0">I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence.</p>
-								</div>
-							</div>
-						</div>
-
-						<div className="col-sm-6">
-							<div className="post post-grid rounded bordered">
-								<div className="thumb top-rounded">
-									<a href="category.html" className="category-badge position-absolute">Fashion</a>
-									<a href="blog-single.html">
-										<div className="inner">
-											<img src="https://katen-template.netlify.app/html/images/posts/post-md-1.jpg" alt="post-title" />
-										</div>
-									</a>
-								</div>
-								<div className="details">
-									<ul className="meta list-inline mb-0">
-										<li className="list-inline-item"><a href="#"><img src="images/other/author-sm.png" className="author" alt="author" />Katen Doe</a></li>
-										<li className="list-inline-item">29 March 2021</li>
-									</ul>
-									<h5 className="post-title mb-3 mt-3"><a href="blog-single.html">Wondering How To Make Your Hair Style Rock?</a></h5>
-									<p className="excerpt mb-0">I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence.</p>
-								</div>
-							</div>
-						</div>
-
-						<div className="col-sm-6">
-							<div className="post post-grid rounded bordered">
-								<div className="thumb top-rounded">
-									<a href="category.html" className="category-badge position-absolute">How To</a>
-									<span className="post-format">
-										<i className="icon-picture"></i>
-									</span>
-									<a href="blog-single.html">
-										<div className="inner">
-											<img src="https://katen-template.netlify.app/html/images/posts/post-md-1.jpg" alt="post-title" />
-										</div>
-									</a>
-								</div>
-								<div className="details">
-									<ul className="meta list-inline mb-0">
-										<li className="list-inline-item"><a href="#"><img src="images/other/author-sm.png" className="author" alt="author" />Katen Doe</a></li>
-										<li className="list-inline-item">29 March 2021</li>
-									</ul>
-									<h5 className="post-title mb-3 mt-3"><a href="blog-single.html">How To Make More Construction By Doing Less</a></h5>
-									<p className="excerpt mb-0">I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence.</p>
-								</div>
-							</div>
-						</div>
-
-						<div className="col-sm-6">
-							<div className="post post-grid rounded bordered">
-								<div className="thumb top-rounded">
-									<a href="category.html" className="category-badge position-absolute">Culture</a>
-									<a href="blog-single.html">
-										<div className="inner">
-											<img src="https://katen-template.netlify.app/html/images/posts/post-md-1.jpg" alt="post-title" />
-										</div>
-									</a>
-								</div>
-								<div className="details">
-									<ul className="meta list-inline mb-0">
-										<li className="list-inline-item"><a href="#"><img src="images/other/author-sm.png" className="author" alt="author" />Katen Doe</a></li>
-										<li className="list-inline-item">29 March 2021</li>
-									</ul>
-									<h5 className="post-title mb-3 mt-3"><a href="blog-single.html">An Incredibly Easy Method That Works For All</a></h5>
-									<p className="excerpt mb-0">I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence.</p>
-								</div>
-							</div>
-						</div>
-
-						<div className="col-sm-6">
-							<div className="post post-grid rounded bordered">
-								<div className="thumb top-rounded">
-									<a href="category.html" className="category-badge position-absolute">Inspiration</a>
-									<a href="blog-single.html">
-										<div className="inner">
-											<img src="https://katen-template.netlify.app/html/images/posts/post-md-1.jpg" alt="post-title" />
-										</div>
-									</a>
-								</div>
-								<div className="details">
-									<ul className="meta list-inline mb-0">
-										<li className="list-inline-item"><a href="#"><img src="images/other/author-sm.png" className="author" alt="author" />Katen Doe</a></li>
-										<li className="list-inline-item">29 March 2021</li>
-									</ul>
-									<h5 className="post-title mb-3 mt-3"><a href="blog-single.html">10 Ways To Immediately Start Selling Furniture</a></h5>
-									<p className="excerpt mb-0">I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence.</p>
-								</div>
-							</div>
-						</div>
-
-						<div className="col-sm-6">
-							<div className="post post-grid rounded bordered">
-								<div className="thumb top-rounded">
-									<a href="category.html" className="category-badge position-absolute">Lifestyle</a>
-									<span className="post-format">
-										<i className="icon-earphones"></i>
-									</span>
-									<a href="blog-single.html">
-										<div className="inner">
-											<img src="https://katen-template.netlify.app/html/images/posts/post-md-1.jpg" alt="post-title" />
-										</div>
-									</a>
-								</div>
-								<div className="details">
-									<ul className="meta list-inline mb-0">
-										<li className="list-inline-item"><a href="#"><img src="images/other/author-sm.png" className="author" alt="author" />Katen Doe</a></li>
-										<li className="list-inline-item">29 March 2021</li>
-									</ul>
-									<h5 className="post-title mb-3 mt-3"><a href="blog-single.html">Now You Can Have Your Thoughts Done Safely</a></h5>
-									<p className="excerpt mb-0">I am so happy, my dear friend, so absorbed in the exquisite sense of mere tranquil existence.</p>
-								</div>
-							</div>
-						</div>
-
+						{this.allPosts()}
 					</div>
 				}
 			</>
