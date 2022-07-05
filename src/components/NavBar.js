9be4c6cd11e138ee407bd2b7ad17e24cd0144bf3
4ydogan <mustafaaydogan12@outlook.com>
2022-07-05 13:32:27 +0300

    Some updates

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,54 +7,59 @@ const NavBar = (props) => {
 
 	useEffect(() => {
 		const logo = axios.get(`/images/logo`);
-		setState({logo: logo.data.image});
-	})
+		setState({ logo: logo.data });
+	}, [])
 
 	return (
 		<>
-			<header className="header-default">
-				<nav className="navbar navbar-expand-lg">
-					<div className="container-xl">
-						<a className="navbar-brand" href="/"><img src={state.logo} alt="logo" /></a>
+			{
+				state.logo ?
+					<header className="header-default">
+						<nav className="navbar navbar-expand-lg">
+							<div className="container-xl">
+								<a className="navbar-brand" href="/"><img src={state.logo.image} alt="logo" /></a>
 
-						<div className="collapse navbar-collapse">
-							<ul className="navbar-nav mr-auto">
-								<li className="nav-item">
-									<a className="nav-link" href="/">Home</a>
-								</li>
-								<li className="nav-item">
-									<a className="nav-link" href="/blog">Blog</a>
-								</li>
-								<li className="nav-item">
-									<a className="nav-link" href="/categories">Categories</a>
-								</li>
-								<li className="nav-item">
-									<a className="nav-link" href="/authors">Authors</a>
-								</li>
-							</ul>
-						</div>
+								<div className="collapse navbar-collapse">
+									<ul className="navbar-nav mr-auto">
+										<li className="nav-item">
+											<a className="nav-link" href="/">Home</a>
+										</li>
+										<li className="nav-item">
+											<a className="nav-link" href="/blog">Blog</a>
+										</li>
+										<li className="nav-item">
+											<a className="nav-link" href="/categories">Categories</a>
+										</li>
+										<li className="nav-item">
+											<a className="nav-link" href="/authors">Authors</a>
+										</li>
+									</ul>
+								</div>
 
-						<div className="header-right">
-							<ul className="social-icons list-unstyled list-inline mb-0">
-								<li className="list-inline-item"><a href="#"><i className="fab fa-facebook-f"></i></a></li>
-								<li className="list-inline-item"><a href="#"><i className="fab fa-twitter"></i></a></li>
-								<li className="list-inline-item"><a href="#"><i className="fab fa-instagram"></i></a></li>
-								<li className="list-inline-item"><a href="#"><i className="fab fa-pinterest"></i></a></li>
-								<li className="list-inline-item"><a href="#"><i className="fab fa-medium"></i></a></li>
-								<li className="list-inline-item"><a href="#"><i className="fab fa-youtube"></i></a></li>
-							</ul>
-							<div className="header-buttons">
-								<button className="search icon-button" onClick={() => props.setSearch(true)}>
-									<i className="icon-magnifier"></i>
-								</button>
-								<button className="burger-menu icon-button" onClick={() => props.setCanvas(true)}>
-									<span className="burger-icon"></span>
-								</button>
+								<div className="header-right">
+									<ul className="social-icons list-unstyled list-inline mb-0">
+										<li className="list-inline-item"><a href="#"><i className="fab fa-facebook-f"></i></a></li>
+										<li className="list-inline-item"><a href="#"><i className="fab fa-twitter"></i></a></li>
+										<li className="list-inline-item"><a href="#"><i className="fab fa-instagram"></i></a></li>
+										<li className="list-inline-item"><a href="#"><i className="fab fa-pinterest"></i></a></li>
+										<li className="list-inline-item"><a href="#"><i className="fab fa-medium"></i></a></li>
+										<li className="list-inline-item"><a href="#"><i className="fab fa-youtube"></i></a></li>
+									</ul>
+									<div className="header-buttons">
+										<button className="search icon-button" onClick={() => props.setSearch(true)}>
+											<i className="icon-magnifier"></i>
+										</button>
+										<button className="burger-menu icon-button" onClick={() => props.setCanvas(true)}>
+											<span className="burger-icon"></span>
+										</button>
+									</div>
+								</div>
 							</div>
-						</div>
-					</div>
-				</nav>
-			</header>
+						</nav>
+					</header>
+
+					: null
+			}
 		</>
 	)
 }
